df8544a220ad9303aa69b69996617d1e1979c3c4
Olivier Guévremont <guevremont.o@gmail.com>
2023-03-06 08:51:48 -0500

    Added method to identify main fluid cells that also removes associated cut cells

--- source/fem-dem/gls_sharp_navier_stokes.cc
+++ source/fem-dem/gls_sharp_navier_stokes.cc
@@ -251,7 +251,6 @@ GLSSharpNavierStokesSolver<dim>::generate_cut_cells_map()
   // principal fluid region to its neighbors, until all connected fluid cells
   // taken into account. The remaining fluid cells are considered disconnected,
   // which is also the case for the connected cut cells.
-  std::cout << "We begin removing disconnected fluid parts" << std::endl;
   principal_fluid_map.clear();
   Point<dim> initial_point;
   for (int d = 0; d < dim; d++)
@@ -260,13 +259,9 @@ GLSSharpNavierStokesSolver<dim>::generate_cut_cells_map()
     LetheGridTools::find_cell_around_point_with_tree(this->dof_handler,
                                                      initial_point);
   principal_fluid_map[initial_cell] = false;
-  bool   propagation_in_progress    = true;
-  size_t compteur                   = 0;
+  bool propagation_in_progress      = true;
   while (propagation_in_progress)
     {
-      compteur += 1;
-      std::cout << "Compteur du nombre de propagations: " << compteur
-                << std::endl;
       propagation_in_progress = false; // until proven otherwise
       for (auto iter = principal_fluid_map.begin();
            iter != principal_fluid_map.end();
@@ -290,57 +285,87 @@ GLSSharpNavierStokesSolver<dim>::generate_cut_cells_map()
                   principal_fluid_map
                     .end()) // if the neighbor is not already accounted for
                 {
-                  if (neighbor_cell->is_locally_owned() ||
-                      neighbor_cell->is_ghost())
+                  bool cell_is_cut;
+                  bool cell_inside;
+                  std::tie(cell_is_cut, std::ignore, std::ignore) =
+                    cut_cells_map[neighbor_cell];
+                  bool cell_is_inside;
+                  std::tie(cell_is_inside, std::ignore) =
+                    cells_inside_map[neighbor_cell];
+                  if (!cell_is_cut && !cell_is_inside) // cell_is_fluid
                     {
-                      bool cell_is_cut;
-                      bool cell_inside;
-                      std::tie(cell_is_cut, std::ignore, std::ignore) =
-                        cut_cells_map[neighbor_cell];
-                      bool cell_is_inside;
-                      std::tie(cell_is_inside, std::ignore) =
-                        cells_inside_map[neighbor_cell];
-                      if (!cell_is_cut && !cell_is_inside) // cell_is_fluid
-                        {
-                          propagation_in_progress =
-                            true; // propagation is still going on
-                          principal_fluid_map[neighbor_cell] = false;
-                        }
+                      propagation_in_progress =
+                        true; // propagation is still going on
+                      principal_fluid_map[neighbor_cell] = false;
                     }
                 }
             }
           principal_fluid_map[cell] = true;
         }
     }
-  std::cout << "Propagation is over" << std::endl;
-  size_t      compteur_cell_not_connected = 0;
   const auto &cell_iterator_2 = this->dof_handler.active_cell_iterators();
   for (const auto &cell : cell_iterator_2)
     {
-      if (cell->is_locally_owned() || cell->is_ghost())
+      bool cell_is_cut;
+      std::tie(cell_is_cut, std::ignore, std::ignore) = cut_cells_map[cell];
+      bool cell_is_inside;
+      std::tie(cell_is_inside, std::ignore) = cells_inside_map[cell];
+      if (!cell_is_inside && !cell_is_cut) // cell_is_fluid
         {
-          bool cell_is_cut;
-          std::tie(cell_is_cut, std::ignore, std::ignore) = cut_cells_map[cell];
-          bool cell_is_inside;
-          std::tie(cell_is_inside, std::ignore) = cells_inside_map[cell];
-          if (!cell_is_inside && !cell_is_cut) // cell_is_fluid
+          const auto iterator = principal_fluid_map.find(cell);
+          if (iterator == principal_fluid_map.end())
             {
-              const auto iterator = principal_fluid_map.find(cell);
-              if (iterator == principal_fluid_map.end())
+              // We have found a fluid cell that is not connected to the
+              // principal fluid
+              cells_inside_map[cell] = {true,
+                                        0}; // TODO CHOOSE A BETTER INTEGER
+                                            // Loop over that group of cells
+              // We also loop around this cell to convert the neighboring cut
+              // cells to inside cells
+              std::vector<typename DoFHandler<dim>::active_cell_iterator>
+                active_neighbors_set =
+                  LetheGridTools::find_cells_around_cell<dim>(vertices_to_cell,
+                                                              cell);
+              std::vector<typename DoFHandler<dim>::active_cell_iterator>
+                     cut_neighbors_vector;
+              size_t number_of_neighbors_checked = 0;
+              bool   still_finding_neighbors     = true;
+              while (still_finding_neighbors)
                 {
-                  compteur_cell_not_connected += 1;
-                  std::cout
-                    << "cell is not connected: " << compteur_cell_not_connected
-                    << std::endl;
-                  // We have found a fluid cell that is not connected to the
-                  // principal fluid
-                  cells_inside_map[cell] = {true,
-                                            0}; // TODO CHOOSE A BETTER INTEGER
+                  still_finding_neighbors = false;
+                  for (unsigned int i = 0; i < active_neighbors_set.size(); ++i)
+                    {
+                      auto neighbor_cell = active_neighbors_set[i];
+                      bool neighbor_cell_is_cut;
+                      int  particle_id;
+                      int  count_particles;
+                      std::tie(neighbor_cell_is_cut,
+                               particle_id,
+                               count_particles) = cut_cells_map[neighbor_cell];
+                      if (neighbor_cell_is_cut)
+                        {
+                          still_finding_neighbors = true;
+                          cut_neighbors_vector.push_back(neighbor_cell);
+                          cells_inside_map[neighbor_cell] = {true, 0};
+                          cut_cells_map[neighbor_cell]    = {false,
+                                                          particle_id,
+                                                          count_particles};
+                        }
+                    }
+                  if (number_of_neighbors_checked < cut_neighbors_vector.size())
+                    still_finding_neighbors = true;
+                  if (still_finding_neighbors)
+                    {
+                      active_neighbors_set =
+                        LetheGridTools::find_cells_around_cell<dim>(
+                          vertices_to_cell,
+                          cut_neighbors_vector[number_of_neighbors_checked]);
+                      number_of_neighbors_checked += 1;
+                    }
                 }
             }
         }
     }
-
 
   // Now, we will removed islands of cut cells in the fluid and solid regions
 }
